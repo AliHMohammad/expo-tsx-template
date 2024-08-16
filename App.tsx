2fdc9c9f74e4c32a60a3f392903d081771628297
Ali Mohammad <alihmohammad@hotmail.com>
2024-08-16 15:16:13 +0200

    chore: spacing

--- App.tsx
+++ App.tsx
@@ -1,24 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 import { NativeWindStyleSheet } from "nativewind";
 NativeWindStyleSheet.setOutput({
     default: "native",
 });
 
 export default function App() {
-  return (
-    <View className="flex-1 bg-red-500 items-center justify-center">
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <View className="flex-1 bg-red-500 items-center justify-center">
+            <Text>Open up App.tsx to start working on your app!</Text>
+            <StatusBar style="auto" />
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: "#fff",
+        alignItems: "center",
+        justifyContent: "center",
+    },
 });
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  // Add more paths if necessarry
-  content: ["./App.{js,jsx,ts,tsx}", "./components/*.{js,jsx,ts,tsx}", "./screens/*.{js,jsx,ts,tsx}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
+    // Add more paths if necessarry
+    content: ["./App.{js,jsx,ts,tsx}", "./components/*.{js,jsx,ts,tsx}", "./screens/*.{js,jsx,ts,tsx}"],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+};
