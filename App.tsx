1b6852908e7698d915f4329102ced119e1ca8505
Ali Mohammad <alihmohammad@hotmail.com>
2024-08-16 16:29:38 +0200

    feat: created better template code with usestate

--- App.tsx
+++ App.tsx
@@ -1,15 +1,26 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Button, Text, View } from "react-native";
 import { NativeWindStyleSheet } from "nativewind";
+import React from "react";
 NativeWindStyleSheet.setOutput({
     default: "native",
 });
 
 export default function App() {
+    const [count, setCount] = React.useState(0);
+
     return (
-        <View className="flex-1 bg-red-500 items-center justify-center">
-            <Text>Open up App.tsx to start working on your app!</Text>
-            <StatusBar style="auto" />
+        <View className="flex-1 flex-column items-center justify-around ">
+            <Text className="text-3xl text-center ">Hello Ali</Text>
+            <View className="flex-column items-center justify-around space-y-5">
+                <View>
+                    <Text className="text-xl">Count: {count}</Text>
+                </View>
+                <View className="flex-row ">
+                    <Button color="red" title="Decrease" onPress={() => setCount((prev) => prev - 1)} />
+                    <Button title="Increase" onPress={() => setCount((prev) => prev + 1)} />
+                </View>
+            </View>
         </View>
     );
 }
