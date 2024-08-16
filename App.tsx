5f12ca095fe43dac41f4a0bd690a216f60b1acd3
Ali Mohammad <alihmohammad@hotmail.com>
2024-08-16 15:05:11 +0200

    feat: initial commit

--- App.tsx
+++ App.tsx
@@ -1,9 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NativeWindStyleSheet } from "nativewind";
+NativeWindStyleSheet.setOutput({
+    default: "native",
+});
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <View className="flex-1 bg-red-500 items-center justify-center">
       <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  // Add more paths if necessarry
+  content: ["./App.{js,jsx,ts,tsx}", "./components/*.{js,jsx,ts,tsx}", "./screens/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
